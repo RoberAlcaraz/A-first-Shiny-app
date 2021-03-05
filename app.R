60735e75a2af01c6720ff91c18a0807e095def86
Roberto Alcaraz <roberto@irba.es>
2021-03-05 11:56:38 +0100

    final first page

--- app.R
+++ app.R
@@ -13,12 +13,14 @@ dataPanel = tabPanel(title = "Stat2Data Package",
                          sidebarPanel(selectInput(inputId = "vars", label = "Select the variable", choices = c("CarType"=1, "Price"=2,
                                                                                                                "Age"=3, "Mileage"=4,
                                                                                                                "Car"=5, "Porsche"=6,
-                                                                                                               "Jaguar"=7, "BMW"=8))),
+                                                                                                               "Jaguar"=7, "BMW"=8)),
+                                      HTML(paste0("<ul><li>", code("CarType"), ": BMW, Jaguar, or Porsche.</li><li>", code("Price"), ": Asking price (in $1,000's).</li><li>", code("Age"), ": Age of the car (in years).</li><li>", code("Mileage"), ": Previous miles driven (in 1,000's).</li><li>", code("Car"), ": 0=Porsche, 1=Jaguar and 2=BMW. </li><li>", code("Porsche"), ": Indicator with 1=Porsche and 0=otherwise.</li><li>", code("Jaguar"), ": Indicator with 1=Jaguar and 0=otherwise.</li><li>", code("BMW"), ": Indicator with 1=BMW and 0=otherwise.</li>
+</ul>")),
+                                      verbatimTextOutput("info")),
                          mainPanel(
                              p("In the following presentation we will do a simple analysis of the", code("ThreeCars"), "data set from the", code("Stat2Data"), "package, which compare prices for Porsche, Jaguar, and BMW cars offered for sale at an internet site."),
                              p("Student project data collected from autotrader.com in Spring 2007."),
-                             dataTableOutput("data"),
-                             verbatimTextOutput("info")
+                             dataTableOutput("data")
                          ))
                      )
 
